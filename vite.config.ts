8bd3569549833f876e440d1e8d607cf71e43ec75
darkeering <darkeering@outlook.com>
2024-01-22 11:02:27 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import electron from "vite-plugin-electron";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    electron({
+      entry: "background.js", // 主进程文件
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
